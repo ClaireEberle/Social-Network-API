14286a9562f0c1375d633be22f0749058e26d53c
Claire <claire.3.eberle@gmail.com>
2023-03-13 20:50:53 -0700

    added reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,25 +1,24 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
+const Reaction = require("./Reaction.js")
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required:true,
-            len: [1,280]
-        },
-        createdAt:{
-            type: Date,
-            default: Date.now
-        },
-        username: {
-            type: String,
-            required:true,
-        }
-        // reactions: {
-        //     ['']
-        // }
-    }
-);
-const Thought = model('thought', thoughtSchema);
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    len: [1, 280],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [
+   Reaction
+  ],
+});
+const Thought = model("thought", thoughtSchema);
 
-module.exports = Thought
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,35 +1,33 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 // const isEmail = require('validator');
 
-const userSchema = new Schema(
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    // validate: [isEmail, 'invalid email']
+  },
+  thoughts: [
     {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            // validate: [isEmail, 'invalid email']
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought'
-            },
-        ],
-        friends: [
-            {
-                type:Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ]
-    }
-);
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
-const User = model('user', userSchema)
+const User = model("user", userSchema);
 
 module.exports = User;
